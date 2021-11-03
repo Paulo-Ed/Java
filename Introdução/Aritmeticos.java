0f534f046cba086dac1febbb3d91c97d9fff64e0
Paulo Santos <pauloed_@outlook.com>
2021-11-03 11:43:13 -0300

    Operadores aritméticos

---
+++ Introdução/Aritmeticos.java
@@ -0,0 +1,23 @@
+package Introdução;
+
+public class Aritmeticos {
+    
+    public static void main(String[] args) {
+        int n1, n2, n3, n4;
+        double n5 = 25;
+        double n6 = 25.4;
+        n1 = 20 / 2;
+        n2 = 30 - 100;
+        n3 = 4 + 10;
+        n4 = 5 * n1;
+        n5 = n5 / 2; 
+        n6 = n6 / 2;
+        
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+        System.out.println(n5);
+        System.out.println(n6);
+    } 
+}
