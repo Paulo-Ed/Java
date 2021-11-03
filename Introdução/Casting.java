f006b76a331c03a161350fc62b438076394d2719
Paulo Santos <pauloed_@outlook.com>
2021-11-03 11:42:51 -0300

    Conversão de tipos

---
+++ Introdução/Casting.java
@@ -0,0 +1,10 @@
+package Introdução;
+
+public class Casting {
+    public static void main(String[] args){
+        //Trata-se da conversão de um tipo em outro que não ocorre automaticamente
+        double numero1 = 2987.45;
+        int numero2 = (int) numero1;
+        System.out.println(numero2);
+    }
+}
