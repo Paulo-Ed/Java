57b92f90e0dfc675f7293edac9cba8800cfe06a2
Paulo Santos <pauloed_@outlook.com>
2021-11-03 11:42:04 -0300

    Variáveis

---
+++ Introdução/Variaveis.java
@@ -0,0 +1,11 @@
+package Introdução;
+
+public class Variaveis {
+    //Java é uma linguagem com tipagem estática e também é fortemente tipada
+    public static void main(String[] args) {
+        int idade;
+        idade = 23;    
+        String nome = "Paulo";
+        System.out.println("Meu nome é " + nome + " eu tenho " + idade + " anos");
+    } 
+}
