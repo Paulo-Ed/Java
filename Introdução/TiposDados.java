0f057300d97a7b2fff297ff732148776613fb11c
Paulo Santos <pauloed_@outlook.com>
2021-11-03 11:42:24 -0300

    Tipos de dados

---
+++ Introdução/TiposDados.java
@@ -0,0 +1,17 @@
+package Introdução;
+
+public class TiposDados {
+    public static void main(String[] args){
+        int inteiro = 10;
+        double pontoFlutuante = 25.4;
+        String caractere = "Sou uma cadeia de caracteres";
+        System.out.println("Inteiro: " + inteiro + "Ponto Flutuante: " + pontoFlutuante + "Caractere: " + caractere);
+
+        //Outros tipos
+        float numero0 = 3.1f;
+        long numero1 = 4567;
+        short numero2 = 122;
+        byte numero3 = 46;
+        System.out.println("float: " + numero0 + "Long: " + numero1 + "short: " + numero2 + "byte: " + numero3);
+    }
+}
